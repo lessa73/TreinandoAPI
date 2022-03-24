60f677360c37675c283618f9d16cafffcf3fa5b1
Alexandre Lessa <alexandrelessa73@gmail.com>
2022-03-24 11:45:20 -0300

    Adicionado pasta src routes e index.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require( 'express');
-
+const routes = require('./routes');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
