9487515a85e257a1976d69312ad8f4a6b5c3ffc9
Alexandre Lessa <alexandrelessa73@gmail.com>
2022-03-24 17:00:58 -0300

    end point get e post inseridos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,18 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
+    console.log(req.query)
     res.send('Hello World!')
+});
+
+routes.get('/produto/:id/categoria', (req, res) => {
+    console.log(req.params)
+    res.send('Hello World')
+});
+
+routes.post( '/cadastrar', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
 });
 
 module.exports = routes;
