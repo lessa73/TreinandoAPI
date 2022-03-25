3ea93b0351b1fc2ab1f2fbb029e5e8dfd8aecbc0
Alexandre Lessa <alexandrelessa73@gmail.com>
2022-03-25 14:12:58 -0300

    inserindo DB

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require( 'express');
 const routes = require('./routes');
+
+const db = require('./database/index.js');
+
 const app = express();
 
+db.hasConection();
+
 app.use(express.json());
+
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
-});
+app.listen(3000, () => { console.log('listening on port 3000')});
---
+++ src/database/index.js
@@ -0,0 +1,33 @@
+const sequelize = require('sequelize');
+
+const DB_NAME = 'chefao';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_CONFIG = {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+};
+
+let db = {};
+
+try {
+    db = new sequelize(DB_NAME, DB_USER, DB_PASS, DB_CONFIG);
+}   catch (error) {
+    console.error('Erro ao tentar uma conexão com o banco de dados');
+};
+
+async function hasConection() {
+    try {
+      await db.authenticate();
+      console.log( 'Banco de dados conectado' );
+    } catch (error) {
+      console.error( 'Não conectado ao banco de dados' );
+    }
+};
+
+Object.assign(db, {
+    hasConection,
+});
+
+module.exports = db;
